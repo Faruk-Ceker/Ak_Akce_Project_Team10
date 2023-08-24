46ac77281931e4772b2a33439e41f6121992f102
Faruk-Ceker <farukceker.te@gmail.com>
2023-08-24 23:32:39 +0300

    US-03 Assert eklendi

--- src/MainProject/us_03.java
+++ src/MainProject/us_03.java
@@ -15,36 +15,39 @@ public class us_03 extends BaseDriver {
         driver.get("http://akakce.com/");
         WebElement girisyap=driver.findElement(By.cssSelector("[id='H_rl_v8']> :nth-child(2)"));
         girisyap.click();
-        MyFunc.Wait(5);
+        MyFunc.Wait(2);
 
 
       WebElement eMail=driver.findElement(By.cssSelector("input[id='life']"));
       eMail.sendKeys("hg@gmail.com");
-    MyFunc.Wait(5);
+    MyFunc.Wait(2);
 
 
     WebElement sifre=driver.findElement(By.cssSelector("input[id='lifp']"));
       sifre.sendKeys("123456Vs@");
-    MyFunc.Wait(5);
+    MyFunc.Wait(2);
 
 
 
 
     WebElement loginButton=driver.findElement(By.cssSelector("input[id='lfb']")) ;
       loginButton.click();
-        MyFunc.Wait(5);
+        MyFunc.Wait(2);
 
 
         WebElement accountIcon=driver.findElement(By.cssSelector("[title='Hesabım']"));
 
       accountIcon.click();
 
-        MyFunc.Wait(5);
+        MyFunc.Wait(2);
 
 
         WebElement exit=driver.findElement(By.cssSelector("[href='#Çık']"));
         exit.click();
+    MyFunc.Wait(2);
 
+WebElement girisButton = driver.findElement(By.xpath("//div[@id='H_rl_v8']/a[text()='Giriş Yap']"));
+Assert.assertTrue("Giriş yap butonu görünmedi, çıkış başarısız", girisyap.getText().contains("Giriş Yap"));
 
 
     BekleVeKapat();
